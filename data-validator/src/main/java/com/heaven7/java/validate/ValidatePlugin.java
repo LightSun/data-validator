dc5d6f7a5b21a2d56fe51528564638c4df1be180
heaven7 <18111646505@163.com>
2020-08-19 10:30:54 +0800

    opt

--- data-validator/src/main/java/com/heaven7/java/validate/ValidatePlugin.java
+++ data-validator/src/main/java/com/heaven7/java/validate/ValidatePlugin.java
@@ -2,17 +2,36 @@ package com.heaven7.java.validate;
 
 import java.lang.annotation.Annotation;
 
+/**
+ * the validate plugin.
+ */
 public interface ValidatePlugin {
 
+    /**
+     * get notice message which will be used if validate failed
+     * @param anno yje annotation
+     * @return the message
+     */
     String getMessage(Annotation anno);
-    Validator getValidator(Annotation anno);
 
+    /**
+     * get the validator. should never be null
+     * @param anno the annotation
+     * @return the validator
+     */
+    Validator getValidator(Annotation anno);
     /**
      * get the cache key (often is classname.hash) , which will be cache if return non-null.
      * @param anno the annotation
      * @return the cache key
      */
     Integer getCacheKey(Annotation anno);
+
+    /**
+     * get the order of validate
+     * @param anno the annotation
+     * @return the order
+     */
     int getOrder(Annotation anno);
 
 }
